b5e182e6e14648b0a9c68c952714b0ba5bd09e7b
kaisiq <dahudohu@gmail.com>
2023-03-09 19:44:56 +0200

    rename contact page to php

--- contact.php
+++ contact.php
@@ -1,5 +1,4 @@
 
-
 <!DOCTYPE html>
 <head>
   <title>Титани ООД | Контакти</title>
--- form.php
+++ form.php
@@ -1,5 +1,5 @@
 <?php
-if(array_key_exists('submit_form',$_POST)){
-    echo "gey"; print_r($_POST); echo "gey";
-}
+    if(array_key_exists('submit_form',$_POST)){
+        echo "gey"; print_r($_POST); echo "gey";
+    }
 ?>
